686a0d5b2722e2f60e938ba86aaf8a257111a25e
ditiatev <dityatevyuri@gmail.com>
2022-11-24 19:53:43 +0300

    .

--- scripts/functions_open.r
+++ scripts/functions_open.r
@@ -1,45 +1,58 @@
 ##
 get_df_sheck <- function(start_date = '2022-05-01',end_date = '2022-06-01',
-                         check_sales_start = 0, check_sales_end = 1000000,
-                         check_margin_start = 0, check_margin_end = 1000000,
-                         con_dalion_en) {
+                         con_postgre) {
   
-  request_code <- paste0("SELECT [code_1c_shop],
-[department_name],
-[ds],
-ROUND(SUM([sales_amount_fact]),2) AS sales,
-ROUND(SUM([prime_cost]),2) AS prime,
-COUNT(distinct [guid_check]) AS n_checks,
-SUM([check_time_sec]) AS check_time
+#   request_code <- paste0("SELECT [code_1c_shop],
+# [department_name],
+# [ds],
+# ROUND(SUM([sales_amount_fact]),2) AS sales,
+# ROUND(SUM([prime_cost]),2) AS prime,
+# COUNT(distinct [guid_check]) AS n_checks,
+# SUM([check_time_sec]) AS check_time
+# 
+# FROM(
+# SELECT [code_1c_shop],
+# [department_name],
+# [date] AS ds,
+# [sales_amount_fact],
+# [prime_cost],
+# [guid_check],
+# DATEDIFF(SECOND, [check_time_opening], [check_time_closing]) AS check_time_sec
+# 
+# FROM [dalion_en].[dbo].[sales]
+# WHERE ([date] BETWEEN 'Repl_start_date' AND 'Repl_end_date') 
+# AND ([sales_amount_fact] BETWEEN Repl_chsl0 AND Repl_chsl1)
+# AND (([sales_amount_fact]-[prime_cost]) BETWEEN Repl_chmrg0 AND Repl_chmrg1)) AS df_sales_time
+# 
+# GROUP BY [ds], [code_1c_shop], [department_name]
+# ORDER BY [code_1c_shop], [department_name], [ds]")
+  
+  request_code <- paste0("SELECT code_1c_shop,
+department_name,
+date as ds,
 
-FROM(
-SELECT [code_1c_shop],
-[department_name],
-[date] AS ds,
-[sales_amount_fact],
-[prime_cost],
-[guid_check],
-DATEDIFF(SECOND, [check_time_opening], [check_time_closing]) AS check_time_sec
+SUM(sales_amount_fact) AS sales,
+SUM(prime_cost) AS prime,
+COUNT(distinct guid_check) AS n_checks
 
-FROM [dalion_en].[dbo].[sales]
-WHERE ([date] BETWEEN 'Repl_start_date' AND 'Repl_end_date') 
-AND ([sales_amount_fact] BETWEEN Repl_chsl0 AND Repl_chsl1)
-AND (([sales_amount_fact]-[prime_cost]) BETWEEN Repl_chmrg0 AND Repl_chmrg1)) AS df_sales_time
+FROM public.df_sales	
 
-GROUP BY [ds], [code_1c_shop], [department_name]
-ORDER BY [code_1c_shop], [department_name], [ds]")
+WHERE (date BETWEEN 'Repl_start_date' AND 'Repl_end_date')
+
+GROUP BY ds, code_1c_shop, department_name
+ORDER BY code_1c_shop, department_name")
   
   # date
   request_code <- gsub("Repl_start_date", start_date, request_code)
   request_code <- gsub("Repl_end_date",   end_date,   request_code)
-  # check sale
-  request_code <- gsub("Repl_chsl0",   check_sales_start,   request_code)
-  request_code <- gsub("Repl_chsl1",   check_sales_end,   request_code)
-  # check margin
-  request_code <- gsub("Repl_chmrg0",   check_margin_start,   request_code)
-  request_code <- gsub("Repl_chmrg1",   check_margin_end,   request_code)
+  # # check sale
+  # request_code <- gsub("Repl_chsl0",   check_sales_start,   request_code)
+  # request_code <- gsub("Repl_chsl1",   check_sales_end,   request_code)
+  # # check margin
+  # request_code <- gsub("Repl_chmrg0",   check_margin_start,   request_code)
+  # request_code <- gsub("Repl_chmrg1",   check_margin_end,   request_code)
   
-  df_sheck <- dbGetQuery(con_dalion_en,request_code)
+  df_sheck <- dbGetQuery(con_postgre,request_code)
   df_sheck$ds <- as.Date(df_sheck$ds)
   
   df_sheck <- df_sheck %>%
@@ -96,19 +109,25 @@ ORDER BY [code_1c_shop], [department_name], [ds]")
   return(df_sheck)
 }
 
-get_ipc_sql <- function(con_analytics, ipc_type = 'food', prediciton = FALSE) {
+
+get_ipc_sql <- function(con_postgre, ipc_type = 'food', prediciton = FALSE) {
   
-  request_code <- paste0("SELECT [date]
-,[ipc]
-FROM [analytics].[dbo].[ipc]
-WHERE ([type] = 'Repl_ipc_type') AND ([prediction] = Repl_prediction)")
+#   request_code <- paste0("SELECT [date]
+# ,[ipc]
+# FROM [analytics].[dbo].[ipc]
+# WHERE ([type] = 'Repl_ipc_type') AND ([prediction] = Repl_prediction)")
+  
+  request_code <- paste0("SELECT date
+,ipc
+FROM public.ipc
+WHERE (type = 'Repl_ipc_type') AND (prediction = Repl_prediction)")
   
   # date
   request_code <- gsub("Repl_ipc_type", ipc_type, request_code)
   request_code <- gsub("Repl_prediction", as.integer(prediciton), request_code)
   
   
-  df_ipc <- dbGetQuery(con_analytics, request_code)
+  df_ipc <- dbGetQuery(con_postgre, request_code)
   df_ipc$date <- as.Date(df_ipc$date)
   df_ipc <- df_ipc %>%
     mutate(n_year  = year(date),
@@ -126,17 +145,17 @@ WHERE ([type] = 'Repl_ipc_type') AND ([prediction] = Repl_prediction)")
   return(df_ipc)
 }
 
-get_weather_sql <- function(con_analytics) {
+get_weather_sql <- function(con_postgre) {
   
-  request_code <- paste0("SELECT [date]
-      ,[temp]
-      ,[prediction]
-      ,[pred_date]
-      ,[pred_name]
-  FROM [analytics].[dbo].[weather]")
+  request_code <- paste0("SELECT date
+      ,temp
+      ,prediction
+      ,pred_date
+      ,pred_name
+  FROM public.weather")
   
   
-  df_weather <- dbGetQuery(con_analytics, request_code)
+  df_weather <- dbGetQuery(con_postgre, request_code)
   df_weather$date <- as.Date(df_weather$date)
   df_weather <- df_weather %>%
     mutate(n_year  = year(date),
@@ -161,69 +180,69 @@ get_weather_sql <- function(con_analytics) {
            dplyr::select(date, temp))
 }
 
-get_n_weeks_sql <- function(con_analytics) {
+get_n_weeks_sql <- function(con_postgre) {
   
-  request_code <- paste0("SELECT [date]
-      ,[n_week]
-      ,[n_month]
-      ,[n_year]
-  FROM [analytics].[dbo].[n_weeks]")
+  request_code <- paste0("SELECT date
+      ,n_week
+      ,n_month
+      ,n_year
+  FROM public.n_weeks")
   
   
-  df_n_weeks_sgl      <- dbGetQuery(con_analytics, request_code)
+  df_n_weeks_sgl      <- dbGetQuery(con_postgre, request_code)
   df_n_weeks_sgl$date <- as.Date(df_n_weeks_sgl$date)
   
   return(df_n_weeks_sgl)
 }
 
-get_disasters_sql <- function(con_analytics) {
+get_disasters_sql <- function(con_postgre) {
   
-  request_code <- paste0("SELECT [date]
-      ,[impact_value]
-      ,[disaster_name]
-  FROM [analytics].[dbo].[disasters]")
+  request_code <- paste0("SELECT date
+      ,impact_value
+      ,disaster_name
+  FROM public.disasters")
   
   
-  df_disasters_sql      <- dbGetQuery(con_analytics, request_code)
+  df_disasters_sql      <- dbGetQuery(con_postgre, request_code)
   df_disasters_sql$date <- as.Date(df_disasters_sql$date)
   
   return(df_disasters_sql)
 }
 
-get_hdays_sql <- function(con_analytics) {
+get_hdays_sql <- function(con_postgre) {
   
-  request_code <- paste0("SELECT [holiday]
-	  ,[date] as ds
-      ,[lower_window]
-      ,[upper_window]
-  FROM [analytics].[dbo].[hdays]")
+  request_code <- paste0("SELECT holiday
+	  ,date as ds
+      ,lower_window
+      ,upper_window
+  FROM public.hdays")
   
   
-  df_hdays_sql    <- dbGetQuery(con_analytics, request_code)
+  df_hdays_sql    <- dbGetQuery(con_postgre, request_code)
   df_hdays_sql$ds <- as.Date(df_hdays_sql$ds)
   
   return(df_hdays_sql)
 }
 
-get_plans_sql <- function(con_analytics,
+get_plans_sql <- function(con_postgre,
                           i_pred_date = '2022-06-24',
                           i_pred_name = 'prophet') {
   
-  request_code <- paste0("SELECT [date]
-      ,[shop_code]
-      ,[dept_code]
-      ,[pred_date]
-      ,[pred_name]
-      ,[pred_value]
-      ,[pred_value_low]
-      ,[pred_value_high]
-  FROM [analytics].[dbo].[pred_dept]
-  WHERE [pred_date] = 'Repl_pred_date' AND [pred_name] = 'Repl_pred_name'")
+  request_code <- paste0("SELECT date
+      ,shop_code
+      ,dept_code
+      ,pred_date
+      ,pred_name
+      ,pred_value
+      ,pred_value_low
+      ,pred_value_high
+  FROM public.df_plans
+  WHERE pred_date = 'Repl_pred_date' AND pred_name = 'Repl_pred_name'")
   
   request_code <- gsub("Repl_pred_date", i_pred_date, request_code)
   request_code <- gsub("Repl_pred_name", i_pred_name, request_code)
   
-  df_pred           <- dbGetQuery(con_analytics,request_code)
+  df_pred           <- dbGetQuery(con_postgre,request_code)
   df_pred$date      <- as.Date(df_pred$date)
   df_pred$pred_date <- as.Date(df_pred$pred_date)
   
@@ -232,19 +251,6 @@ get_plans_sql <- function(con_analytics,
   
   return(df_pred)
 }
-
-# get_unique_plans_list_sql <- function(con_analytics) {
-#   
-#   request_code <- paste0("SELECT [pred_name]
-# ,[pred_date]
-# FROM [analytics].[dbo].[pred_dept]
-# GROUP BY [pred_name],[pred_date]")
-#   
-#   df_pred_list <- dbGetQuery(con_analytics,request_code)
-#   df_pred_list$pred_date <- as.character(df_pred_list$pred_date)
-#   
-#   return(df_pred_list)
-# }
 
 get_unique_plans_list_sql <- function(con_postgre) {
   
